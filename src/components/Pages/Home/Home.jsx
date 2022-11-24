4d904f4bc422fc9d538688948fb11739280c8ae2
Ali <AliDev8x@gmail.com>
2022-11-24 08:45:16 +0300

    Update Home.jsx

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Background from './../../../assets/Background.png'
 const Home = () => {
     return(
-        <div className='absolute top-0'>
+        <div className=' relative top-0'>
             <div>
                 <img src={Background} alt='Background'/>
                 <h1>Hello</h1>
