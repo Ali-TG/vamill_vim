a9a571592aae42ee409db6d214e98fa1bf868ae4
Ali <88180863+Ali-TG@users.noreply.github.com>
2022-11-28 02:29:10 +0300

    Update - 7

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import Home from './components/Pages/Home/Home';
 import Shop from './components/Pages/Shop/Shop';
 import About from './components/Pages/About/About';
 import Contact from './components/Pages/Contact/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
     return(
         <div className='min-h-screen relative flex flex-col'>
           <NavBar/>
-            <BrowserRouter>
               <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/shop' element={<Shop/>}/>
                 <Route path='/about' element={<About/>}/>
                 <Route path='/contact' element={<Contact/>}/>
               </Routes>
-            </BrowserRouter>
             <Footer/>
         </div>
     )
